49453c3ec10fb4c0cec3eb6501bb36d74fa959c8
Judith Nwagwu <Judith5nwagwu@gmail.com>
2020-10-08 13:23:24 -0400

    updating server.js file

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const expressHndl = require("express-handlebars");
+
+const app = express();
+
+let PORT = process.env.PORT || 3306;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(process.cwd() + "/public"));
+
+app.engine("handlebars", expressHndl({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(PORT);
